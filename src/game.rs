8cdd09c2c17c9893a290777320009c79a08cbd17
Pavel Vavruska <vavruska.pavel@gmail.com>
2024-05-26 02:27:52 +0200

    Raytracing uses all CPU cores, commented-out 2d infographics (needs to be moved out of rayon section)

--- src/game.rs
+++ src/game.rs
@@ -6,8 +6,11 @@ use crate::enemy::Enemy;
 use crate::player;
 use crate::drawing::draw_rectange;
 use crate::enemy;
+use crate::projectile;
 use piston_window::color::{WHITE, RED, BLUE, GREEN, YELLOW, GRAY};
 use crate::projectile::Projectile;
+use rayon::iter::Enumerate;
+use rayon::prelude::*;
 
 const WINDOW_X_OFFSET: f64 = 950.0; //256;
 const WINDOW_Y_OFFSET: f64 = 300.0; //192;
@@ -96,9 +99,9 @@ impl Game {
 
     /// Draws entire world.
     pub fn compute_one_tick(&mut self, con: &piston_window::Context,
-         g: &mut piston_window::G2d) -> [[usize; 250]; 250] {
+         g: &mut piston_window::G2d) -> Vec<Vec<usize>> {
 
-        let mut result = [[0; 250]; 250];
+        //let mut result = [[0; 250]; 250];
         // Iterate over the world
 
         // player - no need to draw
@@ -106,17 +109,16 @@ impl Game {
         //draw_rectange(BLUE, self.player.x, self.player.y, 10, 3, con, g);
         
         // spawn_new_rays
-        self.player.spawn_new_rays();
+        //self.player.spawn_new_rays();
 
         // projectiles
         //let light_object: Enemy = enemy::Enemy::new(50.0, 50.0, 50.0, 1.0, 1000, enemy::EnemyType::Point);
 
         let object_size = 25.0;
-        
-        
-        for (index_row, projectile_row) in self.player.projectiles.iter_mut().enumerate() {
+
+        let canvas_vec: Vec<Vec<usize>> = self.player.projectiles.par_iter_mut().enumerate().map(|(index_row, projectile_row)| {
+            let mut canvas_line: Vec<usize> = [0; 250].to_vec();
             for (index_column, projectile) in projectile_row.iter_mut().enumerate() {
-                
                 
                 let mut is_enemy_found = false;
                 
@@ -131,139 +133,140 @@ impl Game {
                         6 => MAGENTA,
                         _ => WHITE, */
                         if projectile.x <= -100.0 {
-                            result[index_row][index_column] = 1;
+                            canvas_line[index_column] = 1;
                             if is_enemy_found {
                                 break;
                             }
                             
                         }
                         if projectile.x >= 200.0 {
-                            result[index_row][index_column] = 2;
+                            canvas_line[index_column] = 2;
                             if is_enemy_found {
                                 break;
                             }
                         }
 
                         if projectile.z <= -100.0 {
-                            result[index_row][index_column] = 3;
+                            canvas_line[index_column] = 3;
                             if is_enemy_found {
                                 break;
                             }
                         }
                         if projectile.z >= 400.0 {
-                            result[index_row][index_column] = 4;
+                            canvas_line[index_column] = 4;
                             if is_enemy_found {
                                 break;
                             }
                         }
                         if projectile.y <= -100.0 {
-                            result[index_row][index_column] = 5;
+                            canvas_line[index_column] = 5;
                             if is_enemy_found {
                                 break;
                             }
                         }
                         if projectile.y >= 400.0 {
-                            result[index_row][index_column] = 6;
+                            canvas_line[index_column] = 6;
                             if is_enemy_found {
                                 break;
                             }
                         }
-                                    
-                    if !is_enemy_found {
-                        //draw_rectange( GRAY, projectile.x , projectile.z, 1, 1, con, g); // DEBUG RAYS
 
-                        let mut delta_z = 0.5;
-                        let mut delta_x = projectile.yaw.tan() * delta_z;
-                        let mut delta_y = projectile.pitch.tan() * delta_z;
 
-                        let vec_len = (delta_x.powf(2.0) + delta_y.powf(2.0) + delta_z.powf(2.0)).sqrt();
-
-                        delta_x /= vec_len;
-                        delta_y /= vec_len;
-                        delta_z /= vec_len;
-
-                        projectile.x += delta_x;  //cos or sin
-                        projectile.z += delta_z;
-                        projectile.y += delta_y;
-                    } else {
-                        let mut delta_x = projectile.yaw.cos();
-                        let mut delta_y = projectile.pitch.sin();
-                        let mut delta_z = projectile.yaw.sin();                        
-                        
-                        let vec_len = (delta_x.powf(2.0) + delta_y.powf(2.0) + delta_z.powf(2.0)).sqrt();
-
-                        delta_x /= vec_len;
-                        delta_y /= vec_len;
-                        delta_z /= vec_len;
-
-                        projectile.x += delta_x;
-                        projectile.y += delta_y;
-                        projectile.z += delta_z;
-                        
-                        if index_row == 25 {
-                            // X cor reflected rays
-                            draw_rectange( BLUE, projectile.x + WINDOW_X_OFFSET, projectile.z, 1, 1, con, g);
-                        }
-                        if index_column == 20 {
-                            // Y cor reflected rays
-                            draw_rectange( YELLOW, projectile.y + WINDOW_X_OFFSET + WINDOW_Y_OFFSET, projectile.z, 1, 1, con, g);
-                        }
-                        continue;           
-                    }
-                    
-                    for (index, enemy) in self.enemies.iter_mut().enumerate() {   
-                                                
-                        let enemy_x_moved_tip = enemy.x - self.player.x - (PI/2.0 as f64).cos()*object_size;
-                        let enemy_z_moved_tip = enemy.z - self.player.z - (PI/2.0 as f64).sin()*object_size;
-                        let enemy_y_moved_tip = enemy.y - self.player.y - (PI/2.0 as f64).cos()*object_size;
-                        let enemy_x_moved_core = enemy.x - self.player.x;
-                        let enemy_z_moved_core = enemy.z - self.player.z;
-                        let enemy_y_moved_core = enemy.y - self.player.y;
-                       
-                        let len_from_core = ((enemy_x_moved_core-projectile.x).powf(2.0) + (enemy_y_moved_core-projectile.y).powf(2.0) + (enemy_z_moved_core-projectile.z).powf(2.0)).sqrt();
-
-                        if index_row == 25 {                     
-                            draw_rectange( YELLOW, enemy_x_moved_core + WINDOW_X_OFFSET, enemy_z_moved_core, 1, 1, con, g); // center of circle
-                            draw_rectange( YELLOW, enemy_x_moved_tip + WINDOW_X_OFFSET , enemy_z_moved_tip, 1, 1, con, g); // top point on the circle
-                        }
-                        if index_column == 25 {
-                            draw_rectange( YELLOW, enemy_y_moved_core + WINDOW_X_OFFSET + WINDOW_Y_OFFSET, enemy_z_moved_core, 1, 1, con, g); // center of circle
-                            draw_rectange( YELLOW, enemy_y_moved_tip + WINDOW_X_OFFSET + WINDOW_Y_OFFSET, enemy_z_moved_tip, 1, 1, con, g); // top point on the circle
+                        if !is_enemy_found {
+                            //draw_rectange( GRAY, projectile.x , projectile.z, 1, 1, con, g); // DEBUG RAYS
+    
+                            let mut delta_z = 0.5;
+                            let mut delta_x = projectile.yaw.tan() * delta_z;
+                            let mut delta_y = projectile.pitch.tan() * delta_z;
+    
+                            let vec_len = (delta_x.powf(2.0) + delta_y.powf(2.0) + delta_z.powf(2.0)).sqrt();
+    
+                            delta_x /= vec_len;
+                            delta_y /= vec_len;
+                            delta_z /= vec_len;
+    
+                            projectile.x += delta_x;  //cos or sin
+                            projectile.z += delta_z;
+                            projectile.y += delta_y;
+                        } else {
+                            let mut delta_x = projectile.yaw.cos();
+                            let mut delta_y = projectile.pitch.sin();
+                            let mut delta_z = projectile.yaw.sin();                        
+                            
+                            let vec_len = (delta_x.powf(2.0) + delta_y.powf(2.0) + delta_z.powf(2.0)).sqrt();
+    
+                            delta_x /= vec_len;
+                            delta_y /= vec_len;
+                            delta_z /= vec_len;
+    
+                            projectile.x += delta_x;
+                            projectile.y += delta_y;
+                            projectile.z += delta_z;
+                            
+                            if index_row == 25 {
+                                // X cor reflected rays
+                                //draw_rectange( BLUE, projectile.x + WINDOW_X_OFFSET, projectile.z, 1, 1, con, g);
+                            }
+                            if index_column == 20 {
+                                // Y cor reflected rays
+                                //draw_rectange( YELLOW, projectile.y + WINDOW_X_OFFSET + WINDOW_Y_OFFSET, projectile.z, 1, 1, con, g);
+                            }
+                            continue;           
                         }
                         
-                        if len_from_core + 0.5 >= object_size && len_from_core - 0.5 <= object_size
-                        && !is_enemy_found {
+                        for (index, enemy) in self.enemies.iter().enumerate() {   
+                                                    
+                            let enemy_x_moved_tip = enemy.x - self.player.x - (PI/2.0 as f64).cos()*object_size;
+                            let enemy_z_moved_tip = enemy.z - self.player.z - (PI/2.0 as f64).sin()*object_size;
+                            let enemy_y_moved_tip = enemy.y - self.player.y - (PI/2.0 as f64).cos()*object_size;
+                            let enemy_x_moved_core = enemy.x - self.player.x;
+                            let enemy_z_moved_core = enemy.z - self.player.z;
+                            let enemy_y_moved_core = enemy.y - self.player.y;
+                           
+                            let len_from_core = ((enemy_x_moved_core-projectile.x).powf(2.0) + (enemy_y_moved_core-projectile.y).powf(2.0) + (enemy_z_moved_core-projectile.z).powf(2.0)).sqrt();
+    
                             if index_row == 25 {                     
-                                draw_rectange( RED, enemy_x_moved_core + WINDOW_X_OFFSET, enemy_z_moved_core, 2, 2, con, g); // center of circle
-                                draw_rectange( RED, enemy_x_moved_tip + WINDOW_X_OFFSET , enemy_z_moved_tip, 2, 2, con, g); // top point on the circle
+                                //draw_rectange( YELLOW, enemy_x_moved_core + WINDOW_X_OFFSET, enemy_z_moved_core, 1, 1, con, g); // center of circle
+                                //draw_rectange( YELLOW, enemy_x_moved_tip + WINDOW_X_OFFSET , enemy_z_moved_tip, 1, 1, con, g); // top point on the circle
                             }
                             if index_column == 25 {
-                                draw_rectange( RED, enemy_y_moved_core + WINDOW_X_OFFSET + WINDOW_Y_OFFSET, enemy_z_moved_core, 2, 2, con, g); // center of circle
-                                draw_rectange( RED, enemy_y_moved_tip + WINDOW_X_OFFSET + WINDOW_Y_OFFSET, enemy_z_moved_tip, 2, 2, con, g); // top point on the circle
+                                //draw_rectange( YELLOW, enemy_y_moved_core + WINDOW_X_OFFSET + WINDOW_Y_OFFSET, enemy_z_moved_core, 1, 1, con, g); // center of circle
+                                //draw_rectange( YELLOW, enemy_y_moved_tip + WINDOW_X_OFFSET + WINDOW_Y_OFFSET, enemy_z_moved_tip, 1, 1, con, g); // top point on the circle
                             }
                             
-                            is_enemy_found = true;
-                            projectile.yaw += PI / 2.0; // aim back
-                            projectile.pitch += PI / 2.0; // aim back
-                            
-                            let len_from_tip = ((enemy_x_moved_tip-projectile.x).powf(2.0) + (enemy_y_moved_tip-projectile.y).powf(2.0) + (enemy_z_moved_tip-projectile.z).powf(2.0)).sqrt();
-                            // tilt rays based on side it comes from
-                            if projectile.x > enemy_x_moved_core {
-                                projectile.yaw += PI/object_size*len_from_tip/2.0;
-                            } else {
-                                projectile.yaw -= PI/object_size*len_from_tip/2.0;
+                            if len_from_core + 0.5 >= object_size && len_from_core - 0.5 <= object_size
+                            && !is_enemy_found {
+                                if index_row == 25 {                     
+                                    //draw_rectange( RED, enemy_x_moved_core + WINDOW_X_OFFSET, enemy_z_moved_core, 2, 2, con, g); // center of circle
+                                    //draw_rectange( RED, enemy_x_moved_tip + WINDOW_X_OFFSET , enemy_z_moved_tip, 2, 2, con, g); // top point on the circle
+                                }
+                                if index_column == 25 {
+                                    //draw_rectange( RED, enemy_y_moved_core + WINDOW_X_OFFSET + WINDOW_Y_OFFSET, enemy_z_moved_core, 2, 2, con, g); // center of circle
+                                    //draw_rectange( RED, enemy_y_moved_tip + WINDOW_X_OFFSET + WINDOW_Y_OFFSET, enemy_z_moved_tip, 2, 2, con, g); // top point on the circle
+                                }
+                                
+                                is_enemy_found = true;
+                                projectile.yaw += PI / 2.0; // aim back
+                                projectile.pitch += PI / 2.0; // aim back
+                                
+                                let len_from_tip = ((enemy_x_moved_tip-projectile.x).powf(2.0) + (enemy_y_moved_tip-projectile.y).powf(2.0) + (enemy_z_moved_tip-projectile.z).powf(2.0)).sqrt();
+                                // tilt rays based on side it comes from
+                                if projectile.x > enemy_x_moved_core {
+                                    projectile.yaw += PI/object_size*len_from_tip/2.0;
+                                } else {
+                                    projectile.yaw -= PI/object_size*len_from_tip/2.0;
+                                }
+                                if projectile.y > enemy_y_moved_core {
+                                    projectile.pitch += PI/object_size*len_from_tip/2.0;
+                                } else {
+                                    projectile.pitch -= PI/object_size*len_from_tip/2.0;
+                                }    
                             }
-                            if projectile.y > enemy_y_moved_core {
-                                projectile.pitch += PI/object_size*len_from_tip/2.0;
-                            } else {
-                                projectile.pitch -= PI/object_size*len_from_tip/2.0;
-                            }
-
                         }
-                    }
                 }
             }
-        }
-        result
+            canvas_line
+        }).collect();
+        canvas_vec
     }
 }
